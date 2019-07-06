f521bd0e31323a3d1495260fa462ea216119c024
donnasvinis <dvsvinis@mac.com>
2019-07-05 21:06:35 -0400

    Forgot about gitting

--- AddingMachine/src/app/text-input/text-input.component.ts
+++ AddingMachine/src/app/text-input/text-input.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextInputComponent implements OnInit {
 
+  public inputText: string;
+  public number1: number;
+  public number2: number;
+  public result: number;
+  
+  ops : string[];
+  vals : number[];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  calculatorAdd() {
+    this.result = this.number1 + this.number2;
+  }
+
+  calculatorSubtract() {
+    this.result = this.number1 - this.number2;
+  }
+
+  calculatorMultiply() {
+    this.result = this.number1 * this.number2;
+  }
+
+  calculatorDivide() {
+    this.result = this.number1 / this.number2;
+  }
+  
+
+
+  
 }
